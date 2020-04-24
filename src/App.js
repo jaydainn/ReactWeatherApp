8c585af64d103bbfec0ca3ef6fa8dd6a6335b02a
Jeremy dain <37037667+jaydainn@users.noreply.github.com>
2020-04-24 18:45:36 +0200

    finalcommit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,97 @@
 import React from 'react'
-import Header from "./components/header"
-import ProjectData from "./data/MyProjects"
-import ProjectCard from "./components/ProjectCard"
-
+import { Spinner } from "reactstrap"
+import WeatherCard from "./components/WeatherCard"
+const API_KEY = "c6facafece457d4cbb494c629a9804e9"
+const style = {
+  container:{
+    paddingTop: 100
+  }
+}
 class App extends React.Component {
 
   constructor() {
     super();
     this.state = {
-      data: ProjectData
+      input: "",
+      data: null
 
     }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleClick = this.handleClick.bind(this)
   }
 
-  
+  handleChange(event) {
+    let myinput = event.target.value;
+    this.setState({ input: myinput })
+
+  }
+
+
+   handleClick(){
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=query&appid=key`
+    url = url.replace("query",this.state.input.toLowerCase());
+    url = url.replace("key",API_KEY)
+    fetch(url)
+      .then((response) => {
+       return response.json()
+      })
+      .then((data) => {
+        this.setState({data: data})
+        console.log(this.state.data)
+      } )
+      
+    }
+
 
   
+
+
+
+
   render() {
-    const Projects = this.state.data.map((element) => {
-      return <ProjectCard key={element.id} data={element} />
-    })
-    return (
-      <div>
-        <Header />
-        <div className="row">
-        {Projects}
-        </div>
+    if(this.state.data != null){
+      return (
+        <div style={style.container} className="container text-center">
+        <div className="input-group mb-3 text-center">
+          <input onChange={this.handleChange} type="text" className="form-control" placeholder="City Name" / >
+            <div className="input-group-append">
+              <button onClick={this.handleClick} className="btn btn-warning" type="button"><img height="16" width="16" src="https://img.icons8.com/pastel-glyph/64/000000/search--v2.png"/></button>
+            </div>
+            </div>
+            <WeatherCard data = {this.state.data} />
+            </div>
+       
         
-      </div>
-    )
+      )
 
+    }
+    else {
+      return (
+        <div style={style.container} className=" container text-center">
+        <div className="input-group mb-3 text-center">
+          <input onChange={this.handleChange} type="text" className="form-control" placeholder="City Name" / >
+            <div className="input-group-append">
+              <button onClick={this.handleClick} className="btn btn-warning" type="button"><img height="16" width="16" src="https://img.icons8.com/pastel-glyph/64/000000/search--v2.png"/></button>
+            </div>
+            </div>
+            
+            </div>
+       
+        
+      )
+
+    }
+    
+   
+  
+  
+    
   }
+
+    
+   
+
+  
 
 
 }
---
+++ src/components/WeatherCard.js
@@ -0,0 +1,59 @@
+import React from "react"
+import {Jumbotron} from "reactstrap"
+const style = {
+    jumbotron:{
+        backgroundColor:"#ffffff"
+    }
+}
+function WeatherCard(props){
+    let weatherimg = ""
+    if(props.data.weather[0].main == "Thunderstorm"){
+        weatherimg = "https://ssl.gstatic.com/onebox/weather/64/rain_s_cloudy.png"
+
+    }
+    if(props.data.weather[0].main == "Clouds"){
+        weatherimg = "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+
+    }
+    if(props.data.weather[0].main == "Rain"){
+        weatherimg = "https://ssl.gstatic.com/onebox/weather/64/rain.png"
+
+    }
+    if(props.data.weather[0].main == "Clear" ||  props.data.weather[0].main == "Haze"){
+        weatherimg = "https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+
+    }
+    if(props.data.weather[0].main == "Snow"){
+        weatherimg = "./data/Snow.png"
+
+    }
+
+    return (
+        <div className="text-center">
+        <Jumbotron style={style.jumbotron} className="text-center rounded">
+            <h1>{props.data.name}</h1>
+            <img src={weatherimg} />
+            <h2>{Math.floor(props.data.main.temp -273.15)}°C</h2>
+            <h2>Wind: {props.data.wind.speed} km/h</h2>
+            
+
+        </Jumbotron>
+        </div>
+
+
+    )
+
+
+
+    
+
+
+
+}
+
+
+
+
+
+
+export default WeatherCard
